54cc7c2734da7c5a0a40fb00ad30c2bc43554df8
chnnxyz <s@chnn.xyz>
2023-08-29 16:26:47 -0600

    repo

--- fixed_income_tools/repo/repo.py
+++ fixed_income_tools/repo/repo.py
@@ -1,7 +1,7 @@
 def reverse_repo_trader_profit(p_now: float, 
                               p_maturity: float, 
                               repo_rate: float,
-                              n_days: int) -> float:
+                              n_days: float) -> float:
     '''Calculates the profit for a repo trader
 
     Parameters
@@ -12,7 +12,7 @@ def reverse_repo_trader_profit(p_now: float,
         Price at maturity
     repo_rate: float
         Normalized repo rate (e.g. 0.06 means 6%)
-    n_days: int
+    n_days: float
         Number of days for the repo trade.
     
     Returns
@@ -22,4 +22,34 @@ def reverse_repo_trader_profit(p_now: float,
     '''
     r_i = n_days * repo_rate * p_now / 360
     profit = p_now + r_i - p_maturity
+    return profit
+
+
+def repo_trader_profit(p_now: float, 
+                       p_maturity: float, 
+                       repo_rate: float,
+                       n_days: float,
+                       hair_cut: float = 0.01) -> float:
+    '''Calculates the profit for a repo trader
+
+    Parameters
+    ----------
+    P_now: float
+        Current Price
+    P_maturity: float
+        Price at maturity
+    repo_rate: float
+        Normalized repo rate (e.g. 0.06 means 6%)
+    n_days: float
+        Number of days for the repo trade.
+    hair_cut: float, optional, default=0.01
+        Hair cut percentange on a normalized scale.
+    
+    Returns
+    -------
+    profit: float
+        Profit for the repo trader.
+    '''
+    r_i = n_days * repo_rate * p_now  * (1 - hair_cut) / 360
+    profit = p_maturity - p_now - r_i
     return profit
